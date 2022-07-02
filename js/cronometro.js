1d10487532e59b7c9f8d16280a941a4ba401252a
XiomaraFer <102708588+XiomaraFer@users.noreply.github.com>
2022-07-01 20:34:39 -0500

    cronometro

---
+++ js/cronometro.js
@@ -0,0 +1,84 @@
+const boton1 = document.getElementById("boton1");
+const boton0 = document.getElementById("boton0");
+boton1.addEventListener("click", accion1);
+boton0.addEventListener("click", reiniciar);
+
+const spanMinutos = document.querySelector(".minutos");
+const spanSegundos = document.querySelector(".segundos");
+const spanCentesimas = document.querySelector(".centesimas");
+
+let minutos = 0;
+let segundos = 0;
+let centesimas = 0;
+
+let corriendo = null;
+
+function dibujarTiempo(){
+    spanMinutos.innerHTML = minutos;
+    spanSegundos.innerHTML = segundos;
+    spanCentesimas.innerHTML = centesimas;
+
+}
+
+function reiniciar(){
+    minutos = 0;
+    segundos = 0;
+    centesimas = 0;
+    dibujarTiempo();
+}
+function accion1(){
+    if(corriendo){
+        detener();
+        boton0.disabled = false; 
+    }
+    else{
+        boton0.disabled = true; 
+        iniciar();
+    }
+}
+
+function iniciar(){
+
+    const sumarMinuto = () => {
+
+        if(minutos < 99) minutos++;
+    }
+
+    const sumarSegundo = () => {
+
+        if(segundos === 59){
+            segundos = 0;
+            sumarMinuto();
+        }
+        else{
+            segundos++;
+        }
+    }
+
+    const incrementar = () => {
+
+        if(centesimas === 99){
+            centesimas = 0;
+            sumarSegundo();
+        }
+        else{
+            centesimas++;
+        }
+
+        dibujarTiempo();
+    }
+
+    corriendo = setInterval(incrementar, 10);
+    boton1.innerHTML = "Detener";
+
+}
+
+function detener(){
+
+    clearInterval(corriendo);
+    corriendo = null;
+    boton1.innerHTML = "Iniciar";
+}
+
+dibujarTiempo();
+
